c03d77fe9239e53ce676b38ad1fa080c24d4bb8e
JaxHolliday <jaxholliday@gmail.com>
2025-04-23 18:34:41 -0400

    Tasker add on

---
+++ Models/TaskerItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTasker.Models
+{
+    public class TaskerItem
+    {
+        //GUID is a unique identifier for each task
+        public Guid Id { get; set; }
+
+        //name of task followed by a default
+        [Required(ErrorMessage = "Every task must have a name")]
+        public string Name { get; set; } = "";
+
+        public bool IsComplete { get; set; } = false;
+    }
+}
